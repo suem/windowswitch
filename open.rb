95f5fa2482adb255441f9d4ecccb3ac427054b71
Samuel Ueltschi <samuel.ueltschi@gmail.com>
2012-03-15 21:33:42 +0100

    open previous dir

--- open.rb
+++ open.rb
@@ -11,17 +11,14 @@ def get_user_selection(windows, message)
   IO.popen(dmenu_command.join(" "), "r+") do |f|
     f.puts(windows)
     f.close_write
-    
-    system("xdotool key Down")
-    system("xdotool key Up")
-    
+
     result = f.gets
   end
   result
 end
 
 
-def select_file(basedir)
+def select_file(basedir, previous = nil)
 
   basedir = File.expand_path(basedir)
 
@@ -45,17 +42,23 @@ def select_file(basedir)
   hidden_files.sort_by {|a| a.downcase}
   normal_files.sort_by {|a| a.downcase}
 
+
   selection_list = ["./","../"]
+  selection_list.unshift previous if previous
   selection_list = selection_list + normal_files + hidden_files
-  
-  
+
+
   user_selection = get_user_selection(selection_list, basedir)
 
   if user_selection != nil
     user_selection.chomp!
     if user_selection == "./"
+      save_previous(basedir)
       open_file(basedir)
+    elsif previous != nil && user_selection == previous
+      select_file(previous)
     elsif user_selection.end_with?("/")
+      save_previous(basedir)
       select_file(basedir+'/'+user_selection)
     else
       open_file(basedir+'/'+user_selection)
@@ -64,10 +67,20 @@ def select_file(basedir)
 
 end
 
+def save_previous(file)
+  myfile = File.new(File.expand_path("~/.cache/previouslyopened"), "w").puts(file)
+
+end
+
 def open_file(filename)
   system "#{OPEN_FILE_COMMAND} #{filename}"
 end
 
 
-
-select_file(ARGV[0])
+data = ''
+f = File.open(File.expand_path("~/.cache/previouslyopened"), "r")
+f.each_line do |line|
+  data += line
+end
+data.chomp!
+select_file(ARGV[0],data)
